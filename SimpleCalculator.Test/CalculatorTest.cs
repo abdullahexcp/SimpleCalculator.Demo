dbd15121aea82710d5b7429706761fd6b86b8748
abdullahexcp <abdullah.magdy@excp.sa>
2022-10-17 18:01:10 +0200

    Add project files.

---
+++ SimpleCalculator.Test/CalculatorTest.cs
@@ -0,0 +1,75 @@
+﻿using NUnit.Framework;
+using SimpleCalculator.Demo;
+using System;
+
+namespace SimpleCalculator.Test
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+
+        [Test, Order(1)]
+        public void TestAdd_TwoDoubleNumbers_ReturnsSum()
+        {
+            var result = CreateNewCalculatorInstance().Addition(3.1, 5.8);
+            Assert.AreEqual(8.9, result);
+        }
+
+        [TestCase(1, 2, 3), Order(2)]
+        [TestCase(2, 3, 5)]
+        [TestCase(3, 4, 7)]
+        public void MultipleTest_Addition_ReturnsSum(double first, double second, double expectedresult)
+        {
+            var calculated = CreateNewCalculatorInstance().Addition(first, second);
+            Assert.AreEqual(expectedresult, calculated);
+        }
+
+        //[Ignore("Ignore test")]
+        [Test, Order(3)]
+        public void TestSubtract_ArgumentException()
+        {
+            Assert.Catch<SystemException>(() => CreateNewCalculatorInstance().Subtraction(4, 6));
+            Assert.Throws<ArgumentException>(() => CreateNewCalculatorInstance().Subtraction(4, 6));
+        }
+
+        [TestCase(10, 2, 8), Order(4)]
+        [TestCase(10, -3, 13)]
+        public void MultipleTest_Subtraction_Of_DoubleNumbers(double first, double second, double expectedresult)
+        {
+            var calculated = CreateNewCalculatorInstance().Subtraction(first, second);
+            Assert.AreEqual(expectedresult, calculated);
+        }
+
+        [TestCase(10, -15, -150), Order(5)]
+        [TestCase(0, 0, 0)]
+        [TestCase(0, 4, 0)]
+        public void MultipleTest_Multiplication_Of_DoubleNumbers(double first, double second, double expectedresult)
+        {
+            var calculated = CreateNewCalculatorInstance().Multiplication(first, second);
+            Assert.AreEqual(expectedresult, calculated);
+        }
+
+
+        [Test, Order(6)]
+        public void Test_DivisionByZero_ArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => CreateNewCalculatorInstance().Division(4, 0));
+        }
+
+        [TestCase(10, -5, -2), Order(7)]
+        [TestCase(1.5, 10, 0.15)]
+        [TestCase(0, 4, 0)]
+        public void MultipleTest_Division_Of_DoubleNumbers(double first, double second, double expectedresult)
+        {
+            var calculated = CreateNewCalculatorInstance().Division(first, second);
+            Assert.AreEqual(expectedresult, calculated);
+        }
+
+        //helper methods
+        private Calculator CreateNewCalculatorInstance()
+        {
+            return new Calculator();
+        }
+
+    }
+}
---
+++ SimpleCalculator/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SimpleCalculator.Demo
+{
+    public class Calculator
+    {
+        public double Addition(double first, double second)
+        {
+            return first + second;
+        }
+
+        public double Subtraction(double first, double second)
+        {
+            if (first < second)
+                throw new ArgumentException($"First number {first} is less than second number {second}");
+
+            return first - second;
+        }
+
+        public double Multiplication(double first, double second)
+        {
+            return first * second;
+        }
+
+        public double Division(double first, double second)
+        {
+            if (second == 0)
+            {
+                throw new ArgumentException($"Math Error, Can't divide by {second} as a second number");
+            }
+            else
+            {
+                return first / second;
+            }
+        }
+    }
+}
